9ae581f3bcfde0ca8e23e288c9069abb1ff98894
SofiaBenasse <sofiabenassef@gmail.com>
2026-06-01 22:35:35 -0300

    docs: adiciona novo arquivo ao README

---
+++ operadores logicos.js
@@ -0,0 +1,18 @@
+//Operadores Logicos
+//AND (&&)
+let idade = 18
+let vistoVerificado = true 
+console.log((idade >=18) && (vistoVerificado === true))
+
+let idade2 = 17
+let vistoVerificado2 = true
+console.log((idade2 >=18) && (vistoVerificado2 === true))
+
+let idade3 = 18
+let vistoVerificado3 = false
+console.log((idade3 >=18) && (vistoVerificado3 === true))
+
+let moedasColetadas = 100 
+let item = "estrela"
+let resultado = (moedasColetadas >= 100) && (item === "estrela")
+console.log(resultado)
