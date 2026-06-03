96cce537ea821eb477a9f0d384488073ece11b6c
SofiaBenasse <sofiabenassef@gmail.com>
2026-06-03 16:18:46 -0300

    Atualiza exemplos de operadores logicos

--- operadores logicos.js
+++ operadores logicos.js
@@ -16,3 +16,34 @@ let moedasColetadas = 100
 let item = "estrela"
 let resultado = (moedasColetadas >= 100) && (item === "estrela")
 console.log(resultado)
+
+// OR (||)
+let tempo = "sol"
+let item = "guarda chuva"
+let podeSair = (tempo !== "chuva") || (item === "guarda chuva")
+console.log ("a pessoal pode sair: " + podeSair)
+
+let tempo2 = "chuva"
+let item2 = "guarda chuva"
+let podeSair2 = (tempo2 !== "sol") || (item === "guarda chuva")
+console.log ("a pessoa pode sair: " + podeSair2)
+
+let tempo3 = "chuva"
+let item3 = "pá"
+let podeSair3 = (tempo3 !== "chuva") || (item3 === "guarda chuva")
+console.log ("a pessoa pode sair: " + podeSair3)
+
+//NOT (!) - NEGA A AFIRMAÇÃO 
+let metereologia = "chuva"
+let resultado = metereologia !== "chuva"
+console.log(resultado)
+
+//MUDANÇA POLARIDADE (!)
+let metereologia2 = "chuva"
+let resultado2 = metereologia2 !== "chuva"
+console.log(!resultado2)
+
+let metereologia3 = "chuva"
+let horario = 8
+let resultado3 = (metereologia3 !== "chuva") && (horario > 6)
+console.log(resultado3)
