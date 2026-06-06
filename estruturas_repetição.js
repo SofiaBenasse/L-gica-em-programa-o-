039945a66a084f00f073674a19d013f607f604ac
SofiaBenasse <sofiabenassef@gmail.com>
2026-06-06 12:17:35 -0300

    adiciona arquivo estruturas_repetição ao readme

---
+++ estruturas_repetição.js
@@ -0,0 +1,28 @@
+//ESTRUTURAS DE REPETIÇÃO FOR/ WHILE/ DO WHILE 
+//FOR
+for (let contador = 0; contador < 4; contador++){
+  console.log(contador)
+}
+
+let pontosDeVida = 0
+for (let i = 0; i < 11; i++){
+  pontosDeVida += 1
+  console.log ("Tomou poção mágica " + i)
+ 
+}
+ console.log (pontosDeVida + " Totais") 
+
+ //WHILE
+ let cont = 0
+ while (cont < 3 ){ 
+  cont++
+  console.log("olá")
+ }
+
+
+// DO WHILE 
+let i = 0
+do{
+  i++ 
+  console.log("hello")
+} while (i < 3)
