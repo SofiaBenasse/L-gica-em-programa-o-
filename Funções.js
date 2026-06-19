3b624adb66e334d1ffcb554d4e6a3d4a24f29aef
SofiaBenasse <sofiabenassef@gmail.com>
2026-06-19 16:50:39 -0300

    adiconar arquivo funções

---
+++ Funções.js
@@ -0,0 +1,28 @@
+//Funções
+torrar()
+
+function torrar(){
+  console.log("torrando pão")
+  injetarPao()
+}
+function injetarPao(){
+  console.log("preparando para injetar o pão")
+  console.log("finalizado")
+}
+
+
+mainSaveData()
+function mainSaveData(){
+  getData()
+  checkValues()
+  sendToDatabase()
+}
+function getData(){
+  console.log("pegando dados do usuario")
+}
+function checkValues(){
+  console.log("validando dados")
+}
+function sendToDatabase(){
+  console.log("cadastrando dados")
+}
